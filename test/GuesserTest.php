b27b28e658297cb0171303207519c508e172b06c
nicole.bruni <bruni@multidialogo.it>
2024-09-24 12:39:31 +0200

    MD/LEGALFORM/GUESSER/001 | fixed search acronym not only at the end of company name

--- test/GuesserTest.php
+++ test/GuesserTest.php
@@ -29,9 +29,15 @@ class GuesserTest extends TestCase
             ['Exact acronym match', 'it', 'SEMPROSIO SRL', 'LIMITED-LIABILITY',],
             ['Exact acronym match with trailing space', 'it', 'SEMPROSIO SRL ', 'LIMITED-LIABILITY',],
             ['Exact acronym match case insensitive', 'it', 'SEMPROSIO srl', 'LIMITED-LIABILITY',],
-            ['Exact acronym match dotted version', 'it', 'SEMPROSIO S.R.L.', 'LIMITED-LIABILITY',],
+            ['Exact acronym match version', 'it', 'SEMPROSIO SAS SEMPROSIO', 'LIMITED-PARTNERSHIP',],
+            ['Exact acronym match dotted version', 'it', 'SEMPROSIO S.R.L. SEMPROSIO', 'LIMITED-LIABILITY',],
+            ['Exact acronym match dotted version', 'it', 'SEMPROSIO S.A.S. SEMPROSIO', 'LIMITED-PARTNERSHIP',],
+
             ['Exact contained by another acronym, acronym match','it', 'SEMPROSIO SRLS', 'SIMPLE-LIMITED-LIABILITY',],
+            ['Exact contained by another acronym, acronym match','it', 'SEMPROSIO SRLS SEMPROSIO', 'SIMPLE-LIMITED-LIABILITY',],
+
             ['Do not match when acronym is part of other word','it', 'SEMPROSIOSPA', null,],
+            ['Do not match when acronym is part of other word','it', 'SPASEMPROSIO', null,],
             ['Exact stop word match', 'it', 'SEMPROSIO SOCIETA SEMPLIFICATA A RESPONSABILITA LIMITATA', 'SIMPLE-LIMITED-LIABILITY',],
             ['Exact stop word match with accented chars', 'it', 'SEMPROSIO SOCIETÀ SEMPLIFICATA A RESPONSABILITÀ LIMITATA', 'SIMPLE-LIMITED-LIABILITY',],
             ['Exact stop word match', 'it', 'DITTA INDIVIDUALE DI SEMPROSIO SEMPROSIO', 'SOLE-PROPRIETORSHIP',],
